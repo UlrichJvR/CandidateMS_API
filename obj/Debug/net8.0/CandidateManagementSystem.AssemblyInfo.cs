fcc77a64649bf7ded5078a89ab7cba3feb372192
UlrichJvR <luculrichjvr@gmail.com>
2025-01-09 12:18:20 +0200

    Initial commit for CandidateMS_API

--- obj/Debug/net8.0/CandidateManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/CandidateManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CandidateManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c9ba0f7fb7f330412fe2fd5481bd23fc490d720")]
 [assembly: System.Reflection.AssemblyProductAttribute("CandidateManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CandidateManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
